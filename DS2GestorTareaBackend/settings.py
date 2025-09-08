c3015f0d70e006f8c3df796a7d7a7c5d28df4f09
emaLeal <emanuel.leal@correounivalle.edu.co>
2025-09-08 06:13:05 -0500

    Dominio actualizado

--- DS2GestorTareaBackend/settings.py
+++ DS2GestorTareaBackend/settings.py
@@ -84,7 +84,7 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:4200',
-    'https://ds-2-gestor-tarea-frontend-nwlo2s2gc-leal31s-projects.vercel.app'
+    'https://ds-2-gestor-tarea-frontend.vercel.app/'
 )
 
 ROOT_URLCONF = 'DS2GestorTareaBackend.urls'
